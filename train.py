4a77a41d29234cfc0c502ced71545c2e14c46902
dkx20080629 <161914757+dkx20080629@users.noreply.github.com>
2025-01-26 13:45:43 +0800

    Add files via upload

---
+++ train.py
@@ -0,0 +1,7 @@
+from ultralytics import YOLO
+from torch import cuda
+
+model = YOLO(r"yolo11s.pt")
+model.to("cuda")
+if __name__ == '__main__':
+    model.train(data=fr"D:\Desktop\yolo\trash8000\data.yaml", epochs=2000, imgsz=640, cache=False, device=0)
